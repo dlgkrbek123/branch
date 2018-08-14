662e3209132f15e027cd96b7564920e431ada4b2
hakyu <dlgkrbek123@gmail.com>
2018-08-14 11:24:49 +0900

    컴포넌트A 완성

--- ComponentA.js
+++ ComponentA.js
@@ -4,15 +4,20 @@ export default class ComponentA extends Component{
   constructor(props){
     super(props)
     this.state={
-
-
+      count:1
     }
+  }
+  componentDidMount(){
+    setInterval(()=>{
+      this.setState({
+        count:this.state.count+1
+      })
+    },1000)    
   }
 
   render(){
     return (
-      <div>
-      </div>
+      <div>{this.state.count}</div>
     )
   }
 }
